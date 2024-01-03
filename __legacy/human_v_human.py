08d4efbce6cb945b117bfa1ce361ccf94465df46
Stefan Clarke Work <stefanclarkework@Stefans-MacBook-Pro-2.local>
2024-01-03 23:20:54 +0000

    deleted old files finally

--- __legacy/human_v_human.py
+++
@@ -1,4 +0,0 @@
-from game.command_line_player import CommandLinePlayer
-
-game = CommandLinePlayer()
-game.play()
--- __legacy2/human_v_human.py
+++
@@ -1,5 +0,0 @@
-from pygame_.pygame_player import PygamePlayer
-
-game = PygamePlayer(3, 1)
-game.game.reset(random_positions=True)
-game.play()
