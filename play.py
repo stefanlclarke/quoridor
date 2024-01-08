16e57f0b8795c9aeb964e5e366398f91873fe3dc
Stefan Clarke Work <stefanclarkework@dynamic-oit-ip4-wifirestricted04-10-50-88-245.princeton.edu>
2024-01-08 09:01:14 -0500

    it worksgit add .git add .! (3 by 3)

--- play.py
+++ play.py
@@ -3,13 +3,16 @@ from quoridor_env.agents.random_agent import RandomAgent
 from quoridor_env.agents.shortest_path_agent import ShortestPathAgent
 from baselines.agents.PPO_baseline_agent import PPOBaselineAgent
 from quoridor_env.gym_env import LoadedQuoridorGym
+from quoridor_ai.model import ActorCriticAgent
 
 
 spagent = ShortestPathAgent(0)
 randomagent = RandomAgent()
 ppoagent = PPOBaselineAgent(model_save_loc='/Users/stefanclarkework/Desktop/quoridor/baselines/models/PPO_example5.zip',
                             env=LoadedQuoridorGym(opponent=randomagent))
+acagent = ActorCriticAgent('/Users/stefanclarkework/Desktop/quoridor/quoridor_ai/saves/23069.908165216446',
+                           LoadedQuoridorGym(randomagent))
 
-player = PygamePlayer(agent_1="human", agent_2=ppoagent)
+player = PygamePlayer(agent_1=acagent, agent_2="human")
 
 player.play()
